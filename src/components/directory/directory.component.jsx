6dabd4cbe1417e16880480a7833309103c93150b
Zhibin Zhang <zzhang@guidewire.com>
2020-12-03 10:01:35 -0500

    ZTM added withRouter HigherOrderComponent to menu-item - React Router Dom

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component {
     render() {
         return (
           <div className="directory-menu">
-            {this.state.sections.map(({ title, imageUrl, id, size }) => (
-              <MenuItem key={id} title={title} imageUrl={imageUrl} size={size? size : ''} />
+            {this.state.sections.map(({ id, ...otherSetionProps }) => (
+              <MenuItem key={id} {...otherSetionProps} />
             ))}
           </div>
         );
