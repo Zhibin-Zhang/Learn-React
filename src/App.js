3ac67419f4846ab2c26a1e6ff8d2a3c53c396fed
Zhibin Zhang <zzhang@guidewire.com>
2020-12-10 22:42:18 -0500

    Implemented firebase utils and integrated firebase authentication

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -22,10 +22,23 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(
+          (snapShot) => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            });
+          }
+        );
+      }
+
+      this.setState({ currentUser: userAuth})
     })
   }
 
@@ -40,7 +53,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route paht="/signIn" component={SignInAndSignUpPage} />
+          <Route paht="/signIn" component={SignInAndSignUpPage} />m
         </Switch>
       </div>
     );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,39 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBZ8Wu3jFQsuxHHr__d4J4nWquHDWfIu7k",
-    authDomain: "crwn-db-228df.firebaseapp.com",
-    projectId: "crwn-db-228df",
-    storageBucket: "crwn-db-228df.appspot.com",
-    messagingSenderId: "852025358606",
-    appId: "1:852025358606:web:883720653caf2490727f5f"
+  apiKey: "AIzaSyCXMcpR9lLkfR1goeVQlrPg0MS_rDe6vdA",
+  authDomain: "crwn-shop-a644a.firebaseapp.com",
+  projectId: "crwn-shop-a644a",
+  storageBucket: "crwn-shop-a644a.appspot.com",
+  messagingSenderId: "934489083453",
+  appId: "1:934489083453:web:5affbb394f2097049fe454",
+  measurementId: "G-2DDFEH56F0",
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
